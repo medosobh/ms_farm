cc785cfb0f85c1e41a1e400d6867b294b09cf53f
salma <medo.sobh76@gmail.com>
2022-10-11 19:50:20 +0200

    Add files via upload

--- models/product_category.py
+++ models/product_category.py
@@ -3,11 +3,16 @@ from odoo import fields, models
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
-    _description = 'Product Category'
 
     location_id = fields.Many2one(
         'stock.location',
-        'id',
         string = "Stock Consume Location",
         check_company = True,
         required = True)
+    company_id = fields.Many2one(
+        'res.company',
+        string = 'Company',
+        change_default = True,
+        default = lambda self: self.env.company,
+        required = False,
+        readonly = True)
