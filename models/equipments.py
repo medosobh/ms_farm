3c38d9594d99e6aae73d784544992ef2ec632b25
salma <medo.sobh76@gmail.com>
2022-10-13 22:20:59 +0200

    Add files via upload

--- models/equipments.py
+++ models/equipments.py
@@ -14,7 +14,6 @@ class farm_equipments(models.Model):
 
     # create a related product under equipment category
     # to use in operation order
-
     def create_equipment_product(self):
         # if it is existed and send error message
         new_name = self.code + " " + self.type + " " + self.name
@@ -35,6 +34,34 @@ class farm_equipments(models.Model):
         self.product_id = new_product.id
         return
 
+    def action_action(self):
+        print('action')
+
+    def _compute_order_line_count(self):
+        for rec in self:
+            count = self.env['farm.operations.oline'].search_count([
+                ('equipments_id', '=', rec.id)])
+            rec.order_line_count = count
+        return rec.order_line_count
+
+    def _compute_order_cost(self):
+        for rec in self:
+            cost = sum(
+                self.env['farm.operations.oline'].search([
+                    ('equipments_id', '=', rec.id)
+                ]).mapped('price_subtotal'))
+            rec.order_line_cost = cost
+        return rec.order_line_cost
+
+    def _compute_actual_cost(self):
+        for record in self:
+            costa = sum(
+                self.env['farm.operations.oline'].search([
+                    ('product_id', '=', record.product_id)
+                ]).mapped('price_subtotal'))
+            record.actual_line_cost = costa
+        return record.actual_line_cost
+
     def update_equipment_product_price_unit(self):
         if self.product_id:
             update = self.env['product.template'].browse(
@@ -48,111 +75,72 @@ class farm_equipments(models.Model):
             print(update)
         return update
 
-    def action_equipment_order_list(self):
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Vendor Bills',
-            'res_model': 'account.move',
-            'domain': [('invoice_origin', '=', self.name)],
-            'view_mode': 'tree,form',
-            'context': {},
-            'target': 'current'
-        }
-
-    def _compute_order_line_count(self):
-        for rec in self:
-            count = self.env['farm.operations.oline'].search_count([
-                ('equipments_id', '=', 'id')
-            ])
-        rec.order_line_count = count
-        return rec.order_line_count
-
-    def _compute_order_line_cost(self):
-        for rec in self:
-            cost = sum(
-                self.env['farm.operations.oline'].search([
-                    ('equipments_id', '=', 'id')
-                ]).mapped('price_subtotal'))
-            rec.order_line_cost = cost
-        return rec.order_line_cost
-
-    def _compute_actual_line_cost(self):
-        print('cost')
-        for rec in self:
-            cost = sum(
-                self.env['farm.operations.oline'].search([
-                    ('equipments_id', '=', 'id')
-                ]).mapped('price_subtotal'))
-            rec.actual_line_cost = cost
-        return rec.actual_line_cost
-
-
-code = fields.Char(
-    string = 'Internal Code',
-    required = True)
-name = fields.Char(
-    string = 'Name',
-    required = True)
-description = fields.Text(
-    'description')
-type = fields.Selection([
-    ('device', 'Device'),
-    ('truck', 'Truck'),
-    ('vehicle', 'Vehicle')
-],
-    required = True)
-body_code = fields.Char(
-    string = 'Body Code',
-    required = True)
-product_id = fields.Many2one(
-    'product.product',
-    domain = "[('categ_id', '=', category_id)]",
-    store = True)
-acq_date = fields.Date(
-    string = 'Acquisition Date',
-    required = True)
-active = fields.Boolean(
-    string = "Active",
-    default = True,
-    tracking = True)
-category_id = fields.Many2one(
-    'product.category',
-    required = True,
-    default = lambda self: self.env.ref('ms_farm.product_category_equipment'),
-    string = 'Product Category')
-company_id = fields.Many2one(
-    'res.company',
-    string = 'Company',
-    change_default = True,
-    default = lambda self: self.env.company,
-    required = False,
-    readonly = True)
-currency_id = fields.Many2one(
-    'res.currency',
-    'Currency',
-    related = 'company_id.currency_id',
-    readonly = True,
-    ondelete = 'set null',
-    help = "Used to display the currency when tracking monetary values")
-buy_sell_price = fields.Float(
-    string = 'buy  and sell price',
-    store = True)
-order_line_count = fields.Integer(
-    string = 'Orders Count',
-    # compute = '_compute_order_line_count',
-    default = 10)
-order_line_cost = fields.Monetary(
-    string = 'total orders',
-    # compute = '_compute_order_line_cost',
-    currency_field = 'currency_id')
-actual_line_cost = fields.Monetary(
-    string = 'total orders',
-    # compute = '_compute_actual_line_cost',
-    currency_field = 'currency_id')
-operation_order_line_ids = fields.One2many(
-    'farm.operations.oline',
-    'equipments_id',
-    string = "order lines")
+    code = fields.Char(
+        string = 'Internal Code',
+        required = True)
+    name = fields.Char(
+        string = 'Name',
+        required = True)
+    description = fields.Text(
+        'description')
+    type = fields.Selection([
+        ('device', 'Device'),
+        ('truck', 'Truck'),
+        ('vehicle', 'Vehicle')
+    ],
+        required = True)
+    body_code = fields.Char(
+        string = 'Body Code',
+        required = True)
+    product_id = fields.Many2one(
+        'product.product',
+        domain = "[('categ_id', '=', category_id)]",
+        store = True)
+    acq_date = fields.Date(
+        string = 'Acquisition Date',
+        required = True)
+    active = fields.Boolean(
+        string = "Active",
+        default = True,
+        tracking = True)
+    category_id = fields.Many2one(
+        'product.category',
+        required = True,
+        default = lambda self: self.env.ref('ms_farm.product_category_equipment'),
+        string = 'Product Category')
+    company_id = fields.Many2one(
+        'res.company',
+        string = 'Company',
+        change_default = True,
+        default = lambda self: self.env.company,
+        required = False,
+        readonly = True)
+    currency_id = fields.Many2one(
+        'res.currency',
+        'Currency',
+        related = 'company_id.currency_id',
+        readonly = True,
+        ondelete = 'set null',
+        help = "Used to display the currency when tracking monetary values")
+    buy_sell_price = fields.Float(
+        string = 'buy  and sell price',
+        store = True)
+    order_line_count = fields.Integer(
+        string = '# of Order',
+        compute = '_compute_order_line_count',
+        store = True)
+    order_line_cost = fields.Monetary(
+        string = 'Order Cost',
+        compute = '_compute_order_cost',
+        store = True)
+    actual_line_cost = fields.Monetary(
+        string = 'Actual Cost',
+        compute = '_compute_actual_cost',
+        store = True)
+    operation_order_line_ids = fields.One2many(
+        'farm.operations.oline',
+        'equipments_id',
+        string = "order lines")
 
 
 class ProductTemplate(models.Model):
